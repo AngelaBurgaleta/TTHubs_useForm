4c9cdf991546fb9c7038394d8d24d54fb83fa199
Angela Burgaleta <135bangbur@gmail.com>
2022-04-10 13:33:57 +0200

    Selects aparte

---
+++ src/components/Selects.jsx
@@ -0,0 +1,467 @@
+import React from 'react'
+
+export const citySelect = [
+    { value: "Spain", label: "Spain" },
+    { value: "Italy", label: "Italy" },
+    { value: "Portugal", label: "Portugal" },
+    { value: "Greece", label: "Greece" },
+    { value: "Tunisia", label: "Tunisia" },
+    { value: "Near East (Jordan, Egypt)", label: "Near East (Jordan, Egypt)" },
+  ];
+
+export const foodgroupSelect = [
+    {
+      value: "Animal and vegetable oils and fats and their derivatives",
+      label: "Animal and vegetable oils and fats and their derivatives",
+    },
+    { value: "Beverages", label: "Beverages" },
+    {
+      value: "Cereals and their products",
+      label: "Cereals and their products",
+    },
+    { value: "Composite dishes", label: "Composite dishes" },
+    { value: "Eggs and their products", label: "Eggs and their products" },
+    { value: "Fats and oils", label: "Fats and oils" },
+    {
+      value: "Fish, shellfish and their products",
+      label: "Fish, shellfish and their products",
+    },
+    {
+      value: "Fish, shellfish, amphibians, reptiles and invertebrates",
+      label: "Fish, shellfish, amphibians, reptiles and invertebrates",
+    },
+    { value: "Food additives", label: "Food additives" },
+    {
+      value: "Food products for young people",
+      label: "Food products for young people",
+    },
+    { value: "Fruits and fruit products", label: "Fruits and fruit products" },
+    {
+      value: "Fruit and vegetable juices and nectars (including concentrates)",
+      label: "Fruit and vegetable juices and nectars (including concentrates)",
+    },
+    { value: "Fruits and their products", label: "Fruits and their products" },
+    {
+      value: "Legumes, nuts, oilseeds, oilseeds and spices",
+      label: "Legumes, nuts, oilseeds, oilseeds and spices",
+    },
+    {
+      value:
+        "Main isolated ingredients, additives, flavorings, leavening agents and processing aids",
+      label:
+        "Main isolated ingredients, additives, flavorings, leavening agents and processing aids",
+    },
+    { value: "Meat and meat products", label: "Meat and meat products" },
+    { value: "Milk and milk products", label: "Milk and milk products" },
+    {
+      value:
+        "Products for non-standard diets, food replacements and dietary supplements",
+      label:
+        "Products for non-standard diets, food replacements and dietary supplements",
+    },
+    {
+      value: "Pulses, seeds and nuts and their products",
+      label: "Pulses, seeds and nuts and their products",
+    },
+    {
+      value: "Roots, tubers, plantains and their products",
+      label: "Roots, tubers, plantains and their products",
+    },
+    {
+      value: "Seasonings, sauces and condiments",
+      label: "Seasonings, sauces and condiments",
+    },
+    { value: "Spices and condiments", label: "Spices and condiments" },
+    { value: "Sweets and sugars", label: "Sweets and sugars" },
+    {
+      value: "Vegetables and derivatives",
+      label: "Vegetables and derivatives",
+    },
+    {
+      value: "Vegetables and their products",
+      label: "Vegetables and their products",
+    },
+  ];
+
+export const foodsubgroupSelect = [
+    {
+      value: "100% fruit and vegetable juices ",
+      label: "100% fruit and vegetable juices ",
+    },
+    { value: "Alcoholic drinks ", label: "Alcoholic drinks " },
+    {
+      value: "Amphibians, reptiles, snails and insects",
+      label: "Amphibians, reptiles, snails and insects",
+    },
+    {
+      value: "Animal and vegetable oils and fats",
+      label: "Animal and vegetable oils and fats",
+    },
+    { value: "Animal blood", label: "Animal blood" },
+    { value: "Animal fat and oil ", label: "Animal fat and oil " },
+
+    { value: "Animal kidney ", label: "Animal kidney " },
+    { value: "Animal liver ", label: "Animal liver " },
+    {
+      value: "Birds (excluding offal): fresh and processed (excluding dried) ",
+      label: "Birds (excluding offal): fresh and processed (excluding dried) ",
+    },
+    {
+      value: "Bread and similar products",
+      label: "Bread and similar products",
+    },
+    {
+      value: "Bread-based dishes and finger foods ",
+      label: "Bread-based dishes and finger foods ",
+    },
+    { value: "Breakfast Cereals", label: "Breakfast Cereals" },
+
+    { value: "Bulbs ", label: "Bulbs " },
+    {
+      value: "Cassava and similar roots (excluding taro) and their products  ",
+      label: "Cassava and similar roots (excluding taro) and their products  ",
+    },
+    {
+      value: "Cereal-based foods for infants and young children",
+      label: "Cereal-based foods for infants and young children",
+    },
+    {
+      value: "Cereals and Its Primary Derivatives",
+      label: "Cereals and Its Primary Derivatives",
+    },
+    { value: "Cheese ", label: "Cheese " },
+    { value: "Chocolate-based sweets ", label: "Chocolate-based sweets " },
+
+    {
+      value: "Concentrated or dehydrated fruit/vegetable juices",
+      label: "Concentrated or dehydrated fruit/vegetable juices",
+    },
+    { value: "Condiments  ", label: "Condiments  " },
+    {
+      value: "Condiments (including table formats)",
+      label: "Condiments (including table formats)",
+    },
+    {
+      value:
+        "Cream, whey and any other milk products excluding fermented milk products and cheese ",
+      label:
+        "Cream, whey and any other milk products excluding fermented milk products and cheese ",
+    },
+    { value: "Crisps and curls ", label: "Crisps and curls " },
+    { value: "Crustaceans ", label: "Crustaceans " },
+
+    {
+      value: "Dairy or dairy imitate based sweets ",
+      label: "Dairy or dairy imitate based sweets ",
+    },
+    {
+      value:
+        "Diadromous fish (excluding offal): fresh and processed (excluding dried)  ",
+      label:
+        "Diadromous fish (excluding offal): fresh and processed (excluding dried)  ",
+    },
+    {
+      value: "Diversos agentes auxiliares para o processamento de alimentos",
+      label: "Diversos agentes auxiliares para o processamento de alimentos",
+    },
+    { value: "Dough-based sweets ", label: "Dough-based sweets " },
+    { value: "Drinking water  ", label: "Drinking water  " },
+    {
+      value: "Eggs: fresh and processed ",
+      label: "Eggs: fresh and processed ",
+    },
+
+    {
+      value: "Extracts of vegetable origin ",
+      label: "Extracts of vegetable origin ",
+    },
+    {
+      value: "Fat emulsions and blended fats",
+      label: "Fat emulsions and blended fats",
+    },
+    { value: "Fermented milk or cream", label: "Fermented milk or cream" },
+    { value: "Fine bakery products", label: "Fine bakery products" },
+    { value: "Fish (muscle) ", label: "Fish (muscle) " },
+    {
+      value: "Fish and seafood-based dishes ",
+      label: "Fish and seafood-based dishes ",
+    },
+
+    {
+      value:
+        "Fish and shellfish - mixed or unspecified: fresh and processed (excluding dried) ",
+      label:
+        "Fish and shellfish - mixed or unspecified: fresh and processed (excluding dried) ",
+    },
+    {
+      value: "Fish and shellfish (including offal) - all types: dried ",
+      label: "Fish and shellfish (including offal) - all types: dried ",
+    },
+    { value: "Fish visceras", label: "Fish visceras" },
+    { value: "Fresh animal fat tissues", label: "Fresh animal fat tissues" },
+    {
+      value:
+        "Freshwater fish (excluding offal): fresh and processed (excluding dried) ",
+      label:
+        "Freshwater fish (excluding offal): fresh and processed (excluding dried) ",
+    },
+    {
+      value: "Fried or extruded cereal, seed or root products",
+      label: "Fried or extruded cereal, seed or root products",
+    },
+
+    {
+      value: "Fried or extruded cereal, seed or root products",
+      label: "Fried or extruded cereal, seed or root products",
+    },
+    { value: "Fruit", label: "Fruit" },
+    {
+      value: "Fruit and nut-based sweets",
+      label: "Fruit and nut-based sweets",
+    },
+    { value: "Fresh animal fat tissues", label: "Fresh animal fat tissues" },
+    {
+      value: "Fruit and vegetable drinks ",
+      label: "Fruit and vegetable drinks ",
+    },
+    {
+      value: "Fruit and vegetable juices and nectars",
+      label: "Fruit and vegetable juices and nectars",
+    },
+
+    { value: "Fruits: dried ", label: "Fruits: dried " },
+    { value: "Fruits: fresh ", label: "Fruits: fresh " },
+    {
+      value: "Fruits: processed (excluding dried and candied) ",
+      label: "Fruits: processed (excluding dried and candied) ",
+    },
+    { value: "Fungi, mosses and lichens", label: "Fungi, mosses and lichens" },
+    {
+      value: "Germinated, sprouted and similar",
+      label: "Germinated, sprouted and similar",
+    },
+    { value: "Herbs and edible flowers", label: "Herbs and edible flowers" },
+
+    { value: "Inflowering cabbage ", label: "Inflowering cabbage " },
+    { value: "Leafy vegetables ", label: "Leafy vegetables " },
+    { value: "Leafy vegetables: fresh ", label: "Leafy vegetables: fresh " },
+    { value: "Legume pods", label: "Legume pods" },
+    { value: "Legume-based dishes ", label: "Legume-based dishes " },
+    { value: "Legumes", label: "Legumes" },
+
+    {
+      value: "Legumes, nuts, oil seeds and spices - Processed",
+      label: "Legumes, nuts, oil seeds and spices - Processed",
+    },
+    {
+      value: "Maize and maize-based products ",
+      label: "Maize and maize-based products ",
+    },
+    {
+      value: "Mammalian and poultry meat",
+      label: "Mammalian and poultry meat",
+    },
+    {
+      value:
+        "Mammals, reptiles and amphibians (excluding offal): fresh and processed (excluding dried) ",
+      label:
+        "Mammals, reptiles and amphibians (excluding offal): fresh and processed (excluding dried) ",
+    },
+    {
+      value:
+        "Marine fish (excluding offal): fresh and processed (excluding dried)",
+      label:
+        "Marine fish (excluding offal): fresh and processed (excluding dried)",
+    },
+    { value: "Meat - all types: dried ", label: "Meat - all types: dried " },
+
+    {
+      value: "Meat and dairy substitutes",
+      label: "Meat and dairy substitutes",
+    },
+    { value: "Meat specialities", label: "Meat specialities" },
+    { value: "Meat-based dishes", label: "Meat-based dishes" },
+    {
+      value: "Microbiological or enzymatic ingredients ",
+      label: "Microbiological or enzymatic ingredients ",
+    },
+    {
+      value: "Milk-based desserts and similar",
+      label: "Milk-based desserts and similar",
+    },
+    {
+      value: "Milk, milk products and concentrates",
+      label: "Milk, milk products and concentrates",
+    },
+
+    { value: "Milk, whey and cream", label: "Milk, whey and cream" },
+    {
+      value:
+        "Milk: fresh and processed (excluding fermented milk products, cream, whey, cheese and other milk products) ",
+      label:
+        "Milk: fresh and processed (excluding fermented milk products, cream, whey, cheese and other milk products) ",
+    },
+    {
+      value: "Millet and millet-based products ",
+      label: "Millet and millet-based products ",
+    },
+    { value: "Mollusks", label: "Mollusks" },
+    {
+      value: "Nuts, seeds and their products ",
+      label: "Nuts, seeds and their products ",
+    },
+    {
+      value: "Offal - all types: fresh and processed (excluding dried) ",
+      label: "Offal - all types: fresh and processed (excluding dried) ",
+    },
+
+    {
+      value: "Oilseeds, oilseeds and oleaginous fruit",
+      label: "Oilseeds, oilseeds and oleaginous fruit",
+    },
+    {
+      value:
+        "Other and unspecified starchy roots and tubers (excluding sugary roots and tubers) and their products",
+      label:
+        "Other and unspecified starchy roots and tubers (excluding sugary roots and tubers) and their products",
+    },
+    { value: "Other animal products", label: "Other animal products" },
+    {
+      value:
+        "Other cereals, mixed cereals or unspecified cereals and their products ",
+      label:
+        "Other cereals, mixed cereals or unspecified cereals and their products ",
+    },
+    {
+      value: "Other non-flavoring food additives, colorings and sweeteners",
+      label: "Other non-flavoring food additives, colorings and sweeteners",
+    },
+    { value: "Other snacks ", label: "Other snacks " },
+
+    { value: "Other sweets ", label: "Other sweets " },
+    {
+      value: "Pasta and noodle-based dishes ",
+      label: "Pasta and noodle-based dishes ",
+    },
+    {
+      value: "Pasta, noodles and similar products",
+      label: "Pasta, noodles and similar products",
+    },
+    {
+      value: "Plates, incl. ready-to-eat meals (excluding soups and salads)",
+      label: "Plates, incl. ready-to-eat meals (excluding soups and salads)",
+    },
+    {
+      value: "Potato, sweet potato and their products ",
+      label: "Potato, sweet potato and their products ",
+    },
+    { value: "Processed eggs", label: "Processed eggs" },
+
+    {
+      value: "Processed fish and fishery products",
+      label: "Processed fish and fishery products",
+    },
+    { value: "Processed Fruit Products", label: "Processed Fruit Products" },
+    { value: "Processed meat products", label: "Processed meat products" },
+    {
+      value: "Processed or preserved vegetables and similar products",
+      label: "Processed or preserved vegetables and similar products",
+    },
+    {
+      value: "Protein isolates and other protein products",
+      label: "Protein isolates and other protein products",
+    },
+    {
+      value: "Pulses (excluding soybeans) and their products",
+      label: "Pulses (excluding soybeans) and their products",
+    },
+
+    {
+      value: "Rice and rice-based products ",
+      label: "Rice and rice-based products ",
+    },
+    {
+      value: "Roots and tubers (excluding starchy and sugary)",
+      label: "Roots and tubers (excluding starchy and sugary)",
+    },
+    {
+      value: "Salty extracts and ingredients for sauces",
+      label: "Salty extracts and ingredients for sauces",
+    },
+    { value: "Sausages ", label: "Sausages " },
+    { value: "Seasonings and extracts", label: "Seasonings and extracts" },
+    {
+      value:
+        "Shellfish (excluding offal) - all types: fresh and processed (excluding dried)",
+      label:
+        "Shellfish (excluding offal) - all types: fresh and processed (excluding dried)",
+    },
+
+    { value: "Soft drink", label: "Soft drink" },
+    {
+      value: "Sorghum and sorghum-based products ",
+      label: "Sorghum and sorghum-based products ",
+    },
+    { value: "Soups ", label: "Soups " },
+    { value: "Soups and salads", label: "Soups and salads" },
+    {
+      value: "Soybean and soy-based products ",
+      label: "Soybean and soy-based products ",
+    },
+    { value: "Spices", label: "Spices" },
+
+    {
+      value: "Spoon desserts and ice cream (generic)",
+      label: "Spoon desserts and ice cream (generic)",
+    },
+    { value: "Starch", label: "Starch" },
+    { value: "Starchy roots and tubers", label: "Starchy roots and tubers" },
+    { value: "Sugars", label: "Sugars" },
+    {
+      value: "Taro and taro-based products ",
+      label: "Taro and taro-based products ",
+    },
+    {
+      value: "Tea, herbal tea, coffee and cocoa ",
+      label: "Tea, herbal tea, coffee and cocoa ",
+    },
+
+    { value: "Unprocessed eggs", label: "Unprocessed eggs" },
+    {
+      value: "Various auxiliary agents for food processing",
+      label: "Various auxiliary agents for food processing",
+    },
+    {
+      value: "Vegetable fat and oil (excluding red palm oil) ",
+      label: "Vegetable fat and oil (excluding red palm oil) ",
+    },
+    { value: "Vegetable fruits", label: "Vegetable fruits" },
+    {
+      value: "Vegetable stems and stems ",
+      label: "Vegetable stems and stems ",
+    },
+    { value: "Vegetable-based dishes ", label: "Vegetable-based dishes " },
+
+    {
+      value:
+        "Vegetables - all types, mixed and unspecified: processed (excluding dried) ",
+      label:
+        "Vegetables - all types, mixed and unspecified: processed (excluding dried) ",
+    },
+    {
+      value: "Vegetables - all types: dried ",
+      label: "Vegetables - all types: dried ",
+    },
+    {
+      value:
+        "Vegetables (excluding leafy vegetables and including fresh legumes): fresh",
+      label:
+        "Vegetables (excluding leafy vegetables and including fresh legumes): fresh",
+    },
+    { value: "Visceras", label: "Visceras" },
+    {
+      value: "Wheat and wheat-based products ",
+      label: "Wheat and wheat-based products ",
+    },
+    { value: "Others...", label: "Others..." },
+  ];
